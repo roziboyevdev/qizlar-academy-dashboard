b8cb8b1ae8b130819d5bd62e2af74faccf21f993
MuhammadqodirProgrammer <komilovm831@gmail.com>
2025-05-15 13:56:49 +0500

    change api

--- src/modules/meeting/api.ts
+++ src/modules/meeting/api.ts
@@ -2,17 +2,17 @@ import http from "services/api";
 import { IMeetingEditBody, IMeetingInput } from "./types";
 
 export const GetMeetingsList = async () => {
-  return await http.get(`/meeting/`);
+  return await http.get(`/vebinar/`);
 };
 
 export const CreateMeeting = async (values: IMeetingInput) => {
-  return await http.post(`/meeting/`, values);
+  return await http.post(`/vebinar/`, values);
 };
 
 export const EditMeeting = async ({ values, id }: IMeetingEditBody) => {
-  return await http.patch(`/meeting/${id}`, values);
+  return await http.patch(`/vebinar/${id}`, values);
 };
 
 export const DeleteMeeting = async (id: string) => {
-  return await http.delete(`/meeting/${id}`);
+  return await http.delete(`/vebinar/${id}`);
 };
