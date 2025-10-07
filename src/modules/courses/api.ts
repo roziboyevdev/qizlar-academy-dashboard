463dac4f9d8a61a784574008a3269864f46a9425
MuhammadqodirProgrammer <komilovm831@gmail.com>
2025-10-07 15:08:18 +0500

    test commit

--- src/modules/courses/api.ts
+++ src/modules/courses/api.ts
@@ -1,18 +1,18 @@
-import  { httpV2 } from 'services/api';
+import  http, { httpV2 } from 'services/api';
 import type { CourseEditBody, CourseInput } from './types';
 
 export const GetCoursesList = async (pageNumber?: number) => {
-  return await httpV2.get(`/course/`, { params: { pageSize: 30, pageNumber } });
+  return await http.get(`/course/`, { params: { pageSize: 30, pageNumber } });
 };
 
 export const CreateCourse = async (values: CourseInput) => {
-  return await httpV2.post('/course/', values);
+  return await http.post('/course/', values);
 };
 
 export const EditCourse = async ({ values, id }: CourseEditBody) => {
-  return await httpV2.patch(`/course/${id}`, values);
+  return await http.patch(`/course/${id}`, values);
 };
 
 export const DeleteCourse = async (id: string) => {
-  return await httpV2.delete(`/course/${id}`);
+  return await http.delete(`/course/${id}`);
 };
