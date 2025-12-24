9c390bc1dbbcde832296e698de11ef566e875da8
Temurbek <hasanovtemur86@gmail.com>
2025-12-24 12:06:19 +0500

    page count

--- src/modules/courses copy/api.ts
+++ src/modules/courses copy/api.ts
@@ -2,7 +2,7 @@ import http from 'services/api';
 import type { CourseEditBody, CourseInput } from './types';
 
 export const GetCoursesList = async (pageNumber?: number) => {
-  return await http.get(`/course/`, { params: { pageSize: 30, pageNumber } });
+  return await http.get(`/course/`, { params: { pageSize: 100, pageNumber } });
 };
 
 export const CreateCourse = async (values: CourseInput) => {
