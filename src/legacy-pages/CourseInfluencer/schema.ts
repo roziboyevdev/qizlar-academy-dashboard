08067965632effdb0d51c987b559d989b469de4a
Abubakrr Ro'ziboyev <abubakirroziboyev7@gmail.com>
2026-04-21 03:02:46 +0500

    removed admin

--- src/legacy-pages/CourseInfluencer/schema.ts
+++
@@ -1,9 +0,0 @@
-// modules/course-influencer/schema.ts
-import { z } from 'zod';
-
-export const schema = z.object({
-  userId: z.string().min(1, { message: 'Foydalanuvchi talab qilinadi' }),
-  courseId: z.string().min(1, { message: 'Kurs talab qilinadi' }),
-});
-
-export type UseFormSchemaType = z.infer<typeof schema>;
--- src/modules/auth/api.ts
+++
@@ -1,9 +0,0 @@
-import http from 'services/api';
-import { IApi } from './types';
-
-export const SignIn = async (values: IApi.SignIn) => {
-  return await http.post(`/admin/login`, {
-    login: values.credentials,
-    password: values.password,
-  });
-};
